f7abcd4128636db815c8f211930f0b278681cbfe
KonstantinaStrantzali <konstrantz@gmail.com>
2022-10-27 18:54:58 +0000

    add lightOn functionality and test it

--- scripts/game.js
+++ scripts/game.js
@@ -1,14 +1,35 @@
-const game = {
-    score : 0,
-    currentGame : [],
-    playerMove: [],
-    choices: ["button1", "button2","button3","button4"],
+let game = {
+    currentGame: [],
+    playerMoves: [],
+    score: 0,
+    choices: ["button1", "button2", "button3", "button4"]
 };
 
-function newGame () {
+function newGame() {
+    game.currentGame = [];
+    game.playerMoves = [];
     game.score = 0;
-    game.currentGame = []
-    game.playerMove = []
-};
+    showScore();
+    addTurn();
+}
 
-module.exports = {game, newGame} ;
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light");
+    setTimeout(function () {
+        document.getElementById(circ).classList.remove("light");
+    }, 400);
+}
+
+
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn};
